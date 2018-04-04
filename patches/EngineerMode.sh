a866fc5e97cdde2e5a81a9fa942425764fe77c18
seluce <36071735+seluce@users.noreply.github.com>
2018-04-04 07:30:43 +0200

    remove engineermode
    remove engineermode

--- patches/EngineerMode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ../../../..
-cd frameworks/av
-git apply -v ../../device/CUBOT/NOTE_S/patches/EngineerMode#1.patch
-cd ../..
-cd frameworks/base
-git apply -v ../../device/CUBOT/NOTE_S/patches/EngineerMode#2.patch
-cd ../..
