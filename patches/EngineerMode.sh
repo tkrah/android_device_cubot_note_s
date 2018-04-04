49ecdbadb83bc62d44e12d0977163b53079c8fc6
seluce <36071735+seluce@users.noreply.github.com>
2018-04-04 07:28:17 +0200

    fix patch for engineermode
    fix patch for engineermode

--- patches/EngineerMode.sh
+++ patches/EngineerMode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ../../../..
 cd frameworks/av
-git apply -v ../../device/CUBOT/NOTE_S/patches_mtk/EngineerMode#1.patch
+git apply -v ../../device/CUBOT/NOTE_S/patches/EngineerMode#1.patch
 cd ../..
 cd frameworks/base
-git apply -v ../../device/CUBOT/NOTE_S/patches_mtk/EngineerMode#2.patch
+git apply -v ../../device/CUBOT/NOTE_S/patches/EngineerMode#2.patch
 cd ../..
